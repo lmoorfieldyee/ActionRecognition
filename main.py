2bd8d1df36de23634c838a8811cd9a9834d827f1
Liam Moorfield-Yee <lmoorfieldyee@gmail.com>
2023-06-13 15:24:47 -0400

    Worked on the real world application file (Main.py). One major issue impacting real time predictions was that the camera is always storing and making predictions on the last 40 frames. This is not good because if you're hands are by your side (i.e. not making a gesture) then as soon as you raise them to make a gesture, a prediction is occuring. Since the video_frame_list is filled with other non-relevant frames, only the beginning of the action was being passed to the model creating poor results. In order to combat this, the video frame list will only be added to if either wrist is raised above their elbow. This ensures that recording starts only when an action is starting. In addition, if an action is halted midway through (i.e. hand drops below elbow) the video frame list resets. Some additional logic needed to be added to the above step to ensure that the list wasn't reset if Google's model losses tracking of the person momentarily. I solved this by adding a time delay where anytime the wrist is lost or dropped for more than a second, the list resets. This commit also added some additional functionality which allows you to set which model you want to use and automatically updates the rest of the program's settings. Also created a helper function to pass Unity required data.

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ import socket
 NOTE: CHECK WHICH MODEL YOU ARE USING AS EACH MODEL HAS DIFFERENT FEATURES. THIS MEANS THAT YOU WILL NOT ONLY
 NEED TO CHANGE THE MODEL BUT ALSO THE extract_landmark() FUNCTION TO PASS THROUGH CORRECT DATA. DEFAULT
 MODEL IS clf_model3-837-0.09.hdf5. YOU ALSO NEED TO UPDATE THE NOSE LANDMARK DATA BEING PASSED TO UNITY AS IT IS IN 
-A DIFFERENT SPOT FOR EACH DATASET.
+A DIFFERENT SPOT FOR EACH DATASET. A YOU ALSO NEED TO UPDATE THE HANDS_ABOVE_ELBOW() FUNCTION... REALLY NEED TO
+JUST AUTOMATE THIS EUGH.
 
 BELOW IS A BREAKDOWN OF MODEL/EXTRACT_LANDMARK FCT'N PAIRS
 1. actions.h5 -> extract_landmarks() fct'n
@@ -21,27 +22,123 @@ BELOW IS A BREAKDOWN OF MODEL/EXTRACT_LANDMARK FCT'N PAIRS
 BELOW IS A BREAKDOWN OF MODEL/NOSE LANDMARK X,Y PAIRS
 1. actions.h5 -> col index 0 (x); col index 1 (y)
 2. clf_model2-190-0.97.hdf5 (found in model2 folder) -> col index 126 (x); col index 127 (y)
+
+BELOW IS A BREAKDOWN OF MODEL MAPPING FOR HANDS_ABOVE_ELBOW()
+1. actions.h5 -> MODEL NUMBER = 1 (DEFAULT)
+2. clf_model2-190-0.97.hdf5 (found in model2 folder) -> MODEL NUMBER = 2
 """
+
+def hand_above_elbow(landmark_list, model_number=1):
+    """
+    Takes in processed frame landmarks and deep learning model. Calculates the distance between wrist landmarks and
+    their respective elbows. If a wrist is above an elbow then returns True as an action could be taking place.
+    Other-wise returns False.
+
+    :param results: Takes in mediapipe landmark results from processed frame
+    :param model_number: Takes either 1 or 2 which specifies which model you are using.
+    1 for model 'actions.h5' (default) and 2 for 'clf_model2-190-0.97.hdf5'
+    :return: True or False
+    """
+    if model_number == 2:
+        # get elbow landmarks (13 (52 (178) ,53 (179)) & 14 (56 (182) , 57 (183)))
+        left_elbow_y = landmark_list[179]
+        right_elbow_y = landmark_list[183]
+
+        # get wrist landmarks (0,1 & 63,64)
+        left_wrist_y = landmark_list[1]
+        right_wrist_y = landmark_list[64]
+    else:
+        # get elbow landmarks (13 (52 (178) ,53 (179)) & 14 (56 (182) , 57 (183)))
+        left_elbow_y = landmark_list[52]
+        right_elbow_y = landmark_list[56]
+
+        # get wrist landmarks (0,1 & 63,64)
+        left_wrist_y = landmark_list[1537]
+        right_wrist_y = landmark_list[1600]
+
+    if ((left_wrist_y != 0) & (left_wrist_y < left_elbow_y)) | ((right_wrist_y != 0) & (right_wrist_y < right_elbow_y)):
+        return True
+    else:
+        return False
+
+def load_model(model_number=1):
+    if model_number==2:
+        return keras.models.load_model('./model2/clf_model2-190-0.97.hdf5')
+    else:
+        return keras.models.load_model('actions.h5')
+
+def send_unity_data(landmark_list, action_being_performed, data_port, model_number=1):
+    if model_number==2:
+        # extract the nose landmark x & y values to update avatar head location
+        x_nose_landmark = str(landmark_list[126])
+        y_nose_landmark = str(landmark_list[127])
+
+        # data to be sent to unity
+        unity_data = (x_nose_landmark + "," + y_nose_landmark + "," + action_being_performed)
+
+        # send nose location data and action data to unity
+        sock.sendto(str.encode(unity_data), data_port)
+    else:
+        # extract the nose landmark x & y values to update avatar head location
+        x_nose_landmark = str(landmark_list[0])
+        y_nose_landmark = str(landmark_list[1])
+
+        # data to be sent to unity
+        unity_data = (x_nose_landmark + "," + y_nose_landmark + "," + action_being_performed)
+
+        # send nose location data and action data to unity
+        sock.sendto(str.encode(unity_data), data_port)
+
+def percent_action_complete(video_len, frames_per_action, frame_to_write):
+    percent_complete = video_len / frames_per_action
+    cv2.putText(frame_to_write, f"{percent_complete}", (120, 200),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 255, 100), 3, cv2.LINE_AA)
+
+def reset_video_list(video_frame_list, append_time_list, time_delay):
+    # We need to add a time delta as it's quite common for the model to lose track of the hand for a second
+
+    # Check to make sure that frame list is being added to, if not just return lists
+    if len(append_time_list) == 0:
+        return video_frame_list, append_time_list
+    # If actively being added to, if it's been longer than 1 second since last frame reset lists
+    else:
+        if (datetime.now() - append_time_list[-1]) > time_delay:
+            # reset video_frames list to empty
+            video_frame_list, append_time_list = [], []
+            return video_frame_list, append_time_list
+        else:
+            return video_frame_list, append_time_list
 
 # instantiate mediapipe
 model = Pipe()
 
+# set which model you're using
+model_number = 2
+
 # load up action recognition neural network
-clf_model = keras.models.load_model('clf_model2-190-0.97.hdf5')
+clf_model = load_model(model_number)
+
 # connect webcam
 cap = cv2.VideoCapture(0)
 
 # set up list to store rolling video frame landmarks
 video_frames = []
+
+# set up list to store time of last video frame appended
+video_frame_append_time = []
+
 # action performed tracker
 detected_actions = [None]
+
 # timestamp for each action
 action_time = [datetime.now()]
+
 # create time delta between each action (i.e. must wait 5 seconds before next action)
-time_delta = timedelta(seconds=0)
+time_delta = timedelta(seconds=1)
 
 # set threshold for action detection
-threshold = 0.65
+threshold = 0.6
+
 # get class names
 actions = os.listdir('./Processed_Data')
 
@@ -58,7 +155,6 @@ with model.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confi
         width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
         height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-
         # make mediapip detections
         frame, results = model.pose_detection(frame, model=holistic)
 
@@ -71,54 +167,62 @@ with model.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confi
         # initialize action being performed - default is no action
         action_being_performed = 'None'
 
+        # Only start appending video frames if hands are above elbows. If hands are below elbows then we are
+        # assuming that no action is being performed.
+        if hand_above_elbow(lh, model_number):
 
-        # append current frame's landmarks to video frame list
-        video_frames.append(lh)
+            # append current frame's landmarks to video frame list & update time
+            video_frames.append(lh)
+            video_frame_append_time.append(datetime.now())
 
-        # only keep the last 40 frames to do detections
-        video_frames = video_frames[-model.frames_per_seq:]
+            percent_action_complete(len(video_frames), model.frames_per_seq, frame)
 
-        # if video_frames is more than 30 frames, check to see if action is performed
-        if len(video_frames) == model.frames_per_seq:
-            # make action predictions
-            res = clf_model.predict(np.expand_dims(video_frames, axis=0))[0]
+            # only keep the last 40 frames to do detections
+            video_frames = video_frames[-model.frames_per_seq:]
 
-            # If action prediction confidence is larger than threshold, print the action
-            if res[np.argmax(res)] > threshold:
-                # check to see if action is idle
-                if actions[np.argmax(res)] == 'idle':
-                    detected_actions.append(actions[np.argmax(res)])
-                    action_time.append(datetime.now())
-                    video_frames = []
+            # if video_frames is more than 40 frames, check to see if action is performed
+            if len(video_frames) == model.frames_per_seq:
 
-                # if not idle, then check to make sure same action is not performed two times in a row
-                # and wait 1 second before making another action prediction (give user time to reset)
-                else:
-                    # Next action cannot be same as previous action
-                    if actions[np.argmax(res)] != detected_actions[-1]:
-                    #print(action_time[-1])
-                    #print((action_time[-1] + time_delta))
-                    # Must wait x (time_delta) seconds before next action
-                    #if (datetime.now() - action_time[-1]) > time_delta:
+                print('Predicting Action!!!')
+
+                # make action predictions
+                res = clf_model.predict(np.expand_dims(video_frames, axis=0))[0]
+
+                # If action prediction confidence is larger than threshold, print the action
+                if res[np.argmax(res)] > threshold:
+                    # check to see if action is idle
+                    if actions[np.argmax(res)] == 'idle':
                         detected_actions.append(actions[np.argmax(res)])
                         action_time.append(datetime.now())
-                        print(actions[np.argmax(res)])
-                        print(res[np.argmax(res)])
+
+                    # if not idle, then check to make sure same action is not performed two times in a row
+                    # and wait 1 second before making another action prediction (give user time to reset)
+                    else:
+                        # Next action cannot be same as previous action
+                        if actions[np.argmax(res)] != detected_actions[-1]:
+
+                            # append action to detected actioon list
+                            detected_actions.append(actions[np.argmax(res)])
+                            action_time.append(datetime.now())
+                            print(actions[np.argmax(res)])
+                            print(res[np.argmax(res)])
+                            # reset frame list for next action to stop rapid fire predictions
+                            video_frames = []
+                            cv2.putText(frame, f"{actions[np.argmax(res)]}", (120, 200),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 255, 100), 3, cv2.LINE_AA)
+
+                            action_being_performed = actions[np.argmax(res)]
+
+                        # reset video frame after a prediction has been made
                         video_frames = []
-                        cv2.putText(frame, f"{actions[np.argmax(res)]}", (120, 200),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 255, 100), 3, cv2.LINE_AA)
+                        video_frame_append_time = []
 
-                        action_being_performed = actions[np.argmax(res)]
+        # If hands are below elbows then we want to keep the video_frames for the next detection empty
+        else:
+            video_frames, video_frame_append_time = reset_video_list(video_frames, video_frame_append_time, time_delta)
 
-        # extract the nose landmark x & y values to update avatar head location
-        x_nose_landmark = str(lh[126])
-        y_nose_landmark = str(lh[127])
-
-        # data to be sent to unity
-        unity_data = (x_nose_landmark + "," + y_nose_landmark + "," + action_being_performed)
-
-        # send nose location data and action data to unity
-        sock.sendto(str.encode(unity_data), serverAddressPort)
+        # send data to given serverAddressPort
+        send_unity_data(lh, action_being_performed, serverAddressPort, model_number)
 
         # show to frame
         cv2.imshow('Frame', frame)
@@ -127,3 +231,6 @@ with model.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confi
         k = cv2.waitKey(10)
         if k == ord('q'):
             break
+
+cap.release()
+cv2.destroyAllWindows()
